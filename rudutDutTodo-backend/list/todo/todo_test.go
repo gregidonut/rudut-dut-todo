9436ef3c4f2aee7c80668152f393d73ce7ce9b55
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-15 07:52:01 +0800

    test: add initial test for NewProgress constructor
    More of a sanity check that this function produces the correct results
    especially later when i add more logic to it

--- rudutDutTodo-backend/list/todo/todo_test.go
+++ rudutDutTodo-backend/list/todo/todo_test.go
@@ -11,36 +11,20 @@ func TestNewProgress(t *testing.T) {
 		name string
 		want *todo.Progress
 	}{
-		// TODO: Add test cases.
+		{
+			name: "InitialProgressIsTodo",
+			want: &todo.Progress{
+				Todo:       true,
+				InProgress: false,
+				Finished:   false,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := todo.NewProgress(); !reflect.DeepEqual(got, tt.want) {
+			got := todo.NewProgress()
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewProgress() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestNewTodo(t *testing.T) {
-	type args struct {
-		postNumber int
-		id         string
-		title      string
-		content    string
-		date       string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *todo.Todo
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := todo.NewTodo(tt.args.postNumber, tt.args.id, tt.args.title, tt.args.content, tt.args.date); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewTodo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -68,6 +52,30 @@ func TestProgress_makeSureOneOfThree(t *testing.T) {
 			}
 			if err := p.MakeSureOneOfThree(); (err != nil) != tt.wantErr {
 				t.Errorf("makeSureOneOfThree() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNewTodo(t *testing.T) {
+	type args struct {
+		postNumber int
+		id         string
+		title      string
+		content    string
+		date       string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *todo.Todo
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := todo.NewTodo(tt.args.postNumber, tt.args.id, tt.args.title, tt.args.content, tt.args.date); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewTodo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
